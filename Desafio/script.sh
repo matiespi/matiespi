4313f173abaebbccf3c1c53fa6d68c6c46af15e1
matiespi <matiasrv96@gmail.com>
2024-03-19 20:32:45 -0300

    update

--- Desafio/script.sh
+++ Desafio/script.sh
@@ -4,14 +4,14 @@ cod=$1
 nom_ape=$2
 departamento=$3
 
-sudo groupadd $departamento
+sudo groupadd ${departamento}
 
 temporalPassword=$(openssl rand -base64 12 | tr -d '\n')
 
-echo "$temporalPassword"
+echo "${temporalPassword}"
 
-sudo useradd -m -c "$nom_ape, $departamento" $cod
+sudo useradd -m -c "$´{nom_ape}, ${departamento}" "${cod}"
 
-sudo echo "$cod:$temporalPassword" | sudo chpasswd
+sudo echo "${cod}:${temporalPassword}" | sudo chpasswd
 
-sudo usermod -aG $departamento $cod 
+sudo usermod -aG ${departamento} ${cod} 
