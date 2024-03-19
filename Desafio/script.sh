cdd6047359d07a8079cf75efe785231975d5430b
matiespi <matiasrv96@gmail.com>
2024-03-19 19:21:51 -0300

    update

---
+++ Desafio/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cod=$1
+nom_ape=$2
+departamento=$3
+
+groupadd departamento
+
+temporalPassword=$(openssl rand -base64 12 | tr -d '\n')
+
+useradd -m -c "$nom_ape, $departamento" $cod
+
+echo "$cod:$temporalPassword" | sudo chpasswd
+
+usermod -aG $departamento $cod
