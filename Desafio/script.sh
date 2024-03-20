58c3d111db512a1bd307ce3787e3db3b0857f42d
matiespi <matiasrv96@gmail.com>
2024-03-19 22:20:21 -0300

    s

--- Desafio/script.sh
+++ Desafio/script.sh
@@ -2,13 +2,12 @@
 
 nom_ape=$1
 departamento=$2
+temporalPassword=$3
 
 var_minus="${nom_ape,,}"
 var_guion="${var_minus// /_}"
 
 sudo groupadd ${departamento}
-
-temporalPassword="1234"
 
 echo "${temporalPassword}"
 
@@ -17,3 +16,5 @@ sudo useradd -m -c "${nom_ape}, ${departamento}" -p $(openssl passwd -1 "$tempor
 echo "${var_guion}:${temporalPassword}" | sudo chpasswd
 
 sudo usermod -aG ${departamento} ${var_guion} 
+
+return ${var_guion}
